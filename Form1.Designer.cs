0f07c2c4d5699f31f627cbf66d0c2f63602e25b6
Ilya2643git <ilyukon2020@gmail.com>
2024-04-08 15:23:47 +0300

    изменения v1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,24 +31,33 @@
             this.glControl1 = new OpenTK.GLControl();
             this.button1 = new System.Windows.Forms.Button();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             this.SuspendLayout();
             // 
             // glControl1
             // 
+            this.glControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.glControl1.BackColor = System.Drawing.Color.Black;
-            this.glControl1.Location = new System.Drawing.Point(1, 51);
+            this.glControl1.Location = new System.Drawing.Point(-10, 128);
+            this.glControl1.Margin = new System.Windows.Forms.Padding(5);
             this.glControl1.Name = "glControl1";
-            this.glControl1.Size = new System.Drawing.Size(884, 613);
+            this.glControl1.Size = new System.Drawing.Size(794, 432);
             this.glControl1.TabIndex = 0;
             this.glControl1.VSync = false;
             this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1, 0);
+            this.button1.Location = new System.Drawing.Point(13, 1);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 45);
+            this.button1.Size = new System.Drawing.Size(100, 25);
             this.button1.TabIndex = 1;
             this.button1.Text = "Открыть";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,25 +65,66 @@
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(82, 0);
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(157, 0);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar1.Maximum = 20;
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(803, 45);
+            this.trackBar1.Size = new System.Drawing.Size(612, 56);
             this.trackBar1.TabIndex = 2;
             this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
+            // trackBar2
+            // 
+            this.trackBar2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar2.Location = new System.Drawing.Point(157, 64);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(611, 56);
+            this.trackBar2.TabIndex = 3;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(14, 36);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(99, 20);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "DrawQuads";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(14, 64);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(111, 20);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "TextureImage";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 661);
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.trackBar2);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.glControl1);
-            this.MinimumSize = new System.Drawing.Size(900, 700);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load_1);
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,6 +135,9 @@
         private OpenTK.GLControl glControl1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenTK;
-using OpenTK.Graphics.OpenGL; 
+using OpenTK.Graphics.OpenGL;
 
 namespace WindowsFormsApp2
 {
@@ -17,7 +17,7 @@ namespace WindowsFormsApp2
         Bin bin = new Bin();
         View view = new View();
         bool loaded = true;
-        int currentLayer;
+        int currentLayer = 0;
         int FrameCount;
         DateTime NextFPSUpdate = DateTime.Now.AddSeconds(1);
         bool needReload = false;
@@ -25,13 +25,17 @@ namespace WindowsFormsApp2
         public Form1()
         {
             InitializeComponent();
-            
+
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            Application.Idle += Application_Idle;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog dialog = new OpenFileDialog();
-            if(dialog.ShowDialog() == DialogResult.OK)
+            if (dialog.ShowDialog() == DialogResult.OK)
             {
                 string str = dialog.FileName;
                 bin.readBIN(str);
@@ -43,29 +47,59 @@ namespace WindowsFormsApp2
 
         private void glControl1_Paint(object sender, PaintEventArgs e)
         {
-            if(loaded)
+            if (loaded)
             {
-                //view.DrawQuads(currentLayer);
-                if(needReload)
+                if (radioButton2.Checked)
                 {
-                    view.generateTextureImage(currentLayer);
-                    view.Load2DTexture();
-                    needReload = false;
+                    if (needReload)
+                    {
+                        view.generateTextureImage(currentLayer);
+                        view.Load2DTexture();
+                        needReload = false;
+                    }
+                    view.DrawTexture();
                 }
-                view.DrawTexture();
-                glControl1.SwapBuffers();
+                if (radioButton1.Checked)
+                    view.DrawQuads(currentLayer);
+
+                glControl1.SwapBuffers();   //загружает наш буфер в буфер экрана.
+            }
+        }
+        private void glControl1_Paint()
+        {
+            if (loaded)
+            {
+                if (radioButton2.Checked)
+                {
+                    if (needReload)
+                    {
+                        view.generateTextureImage(currentLayer);
+                        view.Load2DTexture();
+                        needReload = false;
+                    }
+                    view.DrawTexture();
+                }
+
+                if (radioButton1.Checked)
+                    view.DrawQuads(currentLayer);
+
+                glControl1.SwapBuffers();   //загружает наш буфер в буфер экрана.
             }
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
+            trackBar1.TickFrequency = 1;
+            trackBar1.Minimum = 1;
+            trackBar1.Maximum = Bin.Z - 1;
             currentLayer = trackBar1.Value;
             needReload = true;
+            glControl1_Paint();
         }
 
         void displayFPS()
         {
-            if(DateTime.Now >= NextFPSUpdate)
+            if (DateTime.Now >= NextFPSUpdate)
             {
                 this.Text = String.Format("CT Visualizer (fps = {0})", FrameCount);
                 NextFPSUpdate = DateTime.Now.AddSeconds(1);
@@ -82,9 +116,113 @@ namespace WindowsFormsApp2
             }
         }
 
-        private void Form1_Load_1(object sender, EventArgs e)
+        
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
         {
-            Application.Idle += Application_Idle;
+            trackBar2.TickFrequency = 1;
+            trackBar2.Minimum = 0;
+            trackBar2.Maximum = Bin.Z - 1;
+            view.min = trackBar2.Value;
+            needReload = true;
+            glControl1_Paint();
         }
+
+        
     }
 }
+//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+//using OpenTK;
+//using OpenTK.Graphics.OpenGL;
+
+//namespace WindowsFormsApp2
+//{
+//    public partial class Form1 : Form
+//    {
+//        Bin bin = new Bin();
+//        View view = new View();
+//        bool loaded = true;
+//        int currentLayer, currentLayer2;
+//        int FrameCount;
+//        DateTime NextFPSUpdate = DateTime.Now.AddSeconds(1);
+//        bool needReload = false;
+
+//        public Form1()
+//        {
+//            InitializeComponent();
+
+//        }
+
+//        private void button1_Click(object sender, EventArgs e)
+//        {
+//            OpenFileDialog dialog = new OpenFileDialog();
+//            if (dialog.ShowDialog() == DialogResult.OK)
+//            {
+//                string str = dialog.FileName;
+//                bin.readBIN(str);
+//                view.SetupView(glControl1.Width, glControl1.Height);
+//                loaded = true;
+//                glControl1.Invalidate();
+//            }
+//        }
+
+//        private void glControl1_Paint(object sender, PaintEventArgs e)
+//        {
+//            if (loaded)
+//            {
+//                //view.DrawQuads(currentLayer,currentLayer2);
+//                if (needReload)
+//                {
+//                    view.generateTextureImage(currentLayer, currentLayer2);
+//                    view.Load2DTexture();
+//                    needReload = false;
+//                }
+//                view.DrawTexture();
+//                glControl1.SwapBuffers();
+//            }
+//        }
+
+//        private void trackBar1_Scroll(object sender, EventArgs e)
+//        {
+//            currentLayer = trackBar1.Value;
+//            needReload = true;
+//        }
+//        private void trackBar2_Scroll(object sender, EventArgs e)
+//        {
+//            currentLayer2 = trackBar1.Value;
+//            needReload = true;
+//        }
+
+//        void displayFPS()
+//        {
+//            if (DateTime.Now >= NextFPSUpdate)
+//            {
+//                this.Text = String.Format("CT Visualizer (fps = {0})", FrameCount);
+//                NextFPSUpdate = DateTime.Now.AddSeconds(1);
+//                FrameCount = 0;
+//            }
+//            FrameCount++;
+//        }
+//        void Application_Idle(object sender, EventArgs e)
+//        {
+//            while (glControl1.IsIdle)
+//            {
+//                displayFPS();
+//                glControl1.Invalidate();
+//            }
+//        }
+
+//        private void Form1_Load_1(object sender, EventArgs e)
+//        {
+//            Application.Idle += Application_Idle;
+//        }
+//    }
+//}
